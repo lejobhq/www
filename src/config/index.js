a21fdd09f2502891a0c7f85bbb76d516e96bd264
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2019-05-20 21:52:12 +0800

    Fix production API URL

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const config = {
   api: window.location.href.startsWith("http://localhost")
     ? "http://localhost:3001"
-    : "https://http://lejobhq.appspot.com"
+    : "https://lejobhq.appspot.com"
 };
 
 export default config;
