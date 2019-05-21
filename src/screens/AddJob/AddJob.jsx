146fe539e972fe16e820ccefe874d25f0b6dcba2
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2019-05-22 07:07:06 +0800

    Add styles to add job overlay

--- src/screens/AddJob/AddJob.jsx
+++ src/screens/AddJob/AddJob.jsx
@@ -20,18 +20,18 @@ class AddJob extends Component {
     this.setState({ [name]: value });
   }
 
-  render({ dismissOverlay, onAddNewJob }, { url, notes }) {
+  render({ onDismissOverlay, onAddNewJob }, { url, notes }) {
     return (
       <div class={styles["add-job"]}>
         <div class={styles.close}>
-          <Button type="close" handler={dismissOverlay}>
+          <Button type="close" handler={onDismissOverlay}>
             Close
           </Button>
         </div>
         <h2 class={styles.title}>Add a new job application</h2>
         <div class={styles.form}>
           <label>
-            Job offer link
+            <span>Job offer link</span>
             <input
               type="text"
               name="url"
@@ -41,21 +41,21 @@ class AddJob extends Component {
             />
           </label>
           <label>
-            Notes
+            <span>Notes</span>
             <textarea name="notes" placeholder="(Optional)">
               {notes}
             </textarea>
           </label>
-          <Button
-            type="primary"
-            handler={() => {
-              this.setState({ url: "", notes: "" });
-              onAddNewJob({ url, notes });
-            }}
-          >
-            Add a new job application
-          </Button>
         </div>
+        <Button
+          type="primary"
+          handler={() => {
+            this.setState({ url: "", notes: "" });
+            onAddNewJob({ url, notes });
+          }}
+        >
+          Add a new job application
+        </Button>
       </div>
     );
   }
