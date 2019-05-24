7b1e3218a6eeef39b503496a6df55e41bc706154
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2019-05-25 00:44:38 +0800

    Update show job styles

--- src/screens/ShowJob/ShowJob.jsx
+++ src/screens/ShowJob/ShowJob.jsx
@@ -36,7 +36,11 @@ const ShowJob = ({ job, status, onDismissOverlay, onEditJob }) => (
           <div class={styles.content}>
             <img src={statusToTag(status, job.status)} alt="Tag" />
             <span>
-              {job.status.charAt(0) + job.status.slice(1).toLowerCase()}
+              {job.status.charAt(0) +
+                job.status
+                  .slice(1)
+                  .toLowerCase()
+                  .replace(/_/g, " ")}
             </span>
           </div>
         </div>
@@ -44,49 +48,92 @@ const ShowJob = ({ job, status, onDismissOverlay, onEditJob }) => (
           <p class={styles.label}>Timeline</p>
           <div class={styles.content}>
             <ul>
-              {job.timeline.map(event => (
-                <li>
-                  <span class={styles.date}>
-                    {new Date(event.date).toLocaleDateString()}
-                  </span>
-                  <div class={styles.tag}>
-                    <img src={statusToTag(status, event.status)} alt="Tag" />
-                    <span>
-                      {event.status.charAt(0) +
-                        event.status.slice(1).toLowerCase()}
+              {job.timeline
+                .slice(0)
+                .reverse()
+                .map(event => (
+                  <li>
+                    <span class={styles.date}>
+                      {new Date(event.date).toLocaleDateString()}
                     </span>
-                  </div>
-                </li>
-              ))}
+                    <div>
+                      <div class={styles.tag}>
+                        <img
+                          src={statusToTag(status, event.status)}
+                          alt="Tag"
+                        />
+                        <span>
+                          {event.status.charAt(0) +
+                            event.status
+                              .slice(1)
+                              .toLowerCase()
+                              .replace(/_/g, " ")}
+                        </span>
+                      </div>
+                      {event.metadata ? (
+                        <div class={styles.metadata}>
+                          {event.metadata.notes ? (
+                            <p class={styles["metadata-notes"]}>
+                              {event.metadata.notes}
+                            </p>
+                          ) : null}
+                          {event.metadata.date ? (
+                            <p class={styles["metadata-date"]}>
+                              {new Date(
+                                event.metadata.date
+                              ).toLocaleDateString()}
+                            </p>
+                          ) : null}
+                        </div>
+                      ) : null}
+                    </div>
+                  </li>
+                ))}
             </ul>
           </div>
         </div>
       </div>
       <div class={styles.left}>
-        <div class={styles.technologies}>
-          <p class={styles.label}>Technologies</p>
-          <div class={styles.content}>
-            {job.technologies.map(tech => (
-              <span>{tech}</span>
-            ))}
+        {job.technologies && job.technologies.length ? (
+          <div class={styles.technologies}>
+            <p class={styles.label}>Technologies</p>
+            <div class={styles.content}>
+              {job.technologies.map(tech => (
+                <span>{tech}</span>
+              ))}
+            </div>
           </div>
-        </div>
-        <div class={styles.experience}>
-          <p class={styles.label}>Experience</p>
-          <div class={styles.content}>{job.experience}</div>
-        </div>
-        <div class={styles.visa}>
-          <p class={styles.label}>Visa Sponsorship</p>
-          <div class={styles.content}>{job.visa ? "Yes" : "No"}</div>
-        </div>
-        <div class={styles.relocation}>
-          <p class={styles.label}>Relocation</p>
-          <div class={styles.content}>{job.relocation ? "Yes" : "No"}</div>
-        </div>
-        <div class={styles.size}>
-          <p class={styles.label}>Company Size</p>
-          <div class={styles.content}>{job.company_size}</div>
-        </div>
+        ) : null}
+        {job.experience ? (
+          <div class={styles.experience}>
+            <p class={styles.label}>Experience</p>
+            <div class={styles.content}>{job.experience}</div>
+          </div>
+        ) : null}
+        {job.location ? (
+          <div class={styles.location}>
+            <p class={styles.label}>Office Location</p>
+            <div class={styles.content}>{job.location}</div>
+          </div>
+        ) : null}
+        {job.visa != null ? (
+          <div class={styles.visa}>
+            <p class={styles.label}>Visa Sponsorship</p>
+            <div class={styles.content}>{job.visa ? "Yes" : "No"}</div>
+          </div>
+        ) : null}
+        {job.relocatino != null ? (
+          <div class={styles.relocation}>
+            <p class={styles.label}>Relocation</p>
+            <div class={styles.content}>{job.relocation ? "Yes" : "No"}</div>
+          </div>
+        ) : null}
+        {job.company_size ? (
+          <div class={styles.size}>
+            <p class={styles.label}>Company Size</p>
+            <div class={styles.content}>{job.company_size}</div>
+          </div>
+        ) : null}
       </div>
     </div>
   </div>
